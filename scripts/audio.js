b9dc7228d04edbf9510cb9719442306eb9dccf6b
Shinners888 <sinead888888888@gmail.com>
2021-01-16 16:11:11 +0000

    initial commit with confirmation of audio working

---
+++ scripts/audio.js
@@ -0,0 +1,20 @@
+const ctx = new AudioContext();
+let audio;
+
+fetch("./audioclips/testnote.wav")
+.then(data => data.arrayBuffer())
+
+.then(arrayBuffer => ctx.decodeAudioData(arrayBuffer))
+
+.then(decodedAudio => {
+    audio = decodedAudio
+})
+
+function playback() {
+    const playSound = ctx.createBufferSource();
+    playSound.buffer = audio;
+    playSound.connect(ctx.destination);
+    playSound.start(ctx.currentTime);
+}
+
+window.addEventListener("mousedown", playback);
