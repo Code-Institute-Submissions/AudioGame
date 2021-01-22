7ea190b766b89f5f9ecc81a04bf882c659cd7ac0
Shinners888 <sinead888888888@gmail.com>
2021-01-22 20:03:07 +0000

    uploaded wireframes. changed triggered sound file. tested. works

--- scripts/audio.js
+++ scripts/audio.js
@@ -1,20 +1,37 @@
 const ctx = new AudioContext();
 let audio;
 
+//get minor second file
 fetch("audioclips/minorsecond.wav")
 .then(data => data.arrayBuffer())
 
 .then(arrayBuffer => ctx.decodeAudioData(arrayBuffer))
 
 .then(decodedAudio => {
-    audio = decodedAudio
+    minorSecond = decodedAudio
 })
+//get major second file
+fetch("audioclips/majorsecond.wav")
+.then(data => data.arrayBuffer())
+
+.then(arrayBuffer => ctx.decodeAudioData(arrayBuffer))
+
+.then(decodedAudio => {
+    majorSecond = decodedAudio
+})
+
+
+
+
+
+
 
 function playback() {
     const playSound = ctx.createBufferSource();
-    playSound.buffer = audio;
+    playSound.buffer = majorSecond;
     playSound.connect(ctx.destination);
     playSound.start(ctx.currentTime);
 }
 
 playAgain.addEventListener("mousedown", playback);
+
